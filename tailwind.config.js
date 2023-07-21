f6950544a7f93e7b1784c896a917005519149d5e
konstantinobuhov <obuchov.k@gmail.com>
2023-07-21 19:39:44 +0300

    Fixes

--- tailwind.config.js
+++ tailwind.config.js
@@ -35,10 +35,11 @@ module.exports = {
         "md": "768px",
         "lg": "1024px",
         "xl": "1280px",
-        "2xl": "1440px",
+        "2xl": "1340px",
       },
     },
     plugins: [
+      require('autoprefixer'),
     ],
   },
 };
